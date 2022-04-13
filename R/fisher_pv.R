e33c25d6017c83f9f596bb78eb43c0eba85cfa43
mlysy <mlysy@users.noreply.github.com>
2022-04-13 16:47:36 -0400

    added strict argument to fisher_pv()

--- R/fisher_pv.R
+++ R/fisher_pv.R
@@ -4,23 +4,30 @@
 #' @param group Vector of length `nobs` indicating to which group each observation belongs.  Group membership is determined from the elements of `unique(group)`.
 #' @param Tfun Function taking vector arguments `value` and `group`, returning `ntest` test statistics.
 #' @param nsim Number of randomizations to perform.
+#' @param strict Whether the p-value is "strictly greater" or just "greater than or equal" (see 'Details').
 #' @details Suppose there is only one test statistic `ntest = 1`.  Then the Fisher randomization test calculates
 #' ```
 #' Pr(Tsim > Tobs),
 #' ```
 #' where `Tobs = Tfun(value, group)` is the observed value of the test statistic, and `Tsim = Tfun(value[irand], group)`, where `irand` is a random permutation of the `nobs` observations.  When `ntest > 1`, [fisher_pv()] performs the calculation above for each test statistic.
+#'
+#' When `strict = FALSE` the p-value is `Pr(Tsim >= Tobs)`.
 #' @return A two column matrix with `ntest` rows and columns:
 #' \describe{
 #'   \item{`Tobs`}{A vector of length `ntest`, where `Tobs[t]` is the observed value of test statistic `t`.}
 #'   \item{`pval`}{A vector of `ntest` p-values of the form `Pr(Tsim[t] > Tobs[t])`, where `Tobs[t] = Tfun(value, group)[t]` and `Tsim[t]` is computed by randomly reallocating `value` to the given vector `group`.}
 #' }
 #' @export
-fisher_pv <- function(value, group, Tfun, nsim = 1e3) {
+fisher_pv <- function(value, group, Tfun, nsim = 1e3, strict = TRUE) {
   Tobs <- Tfun(value = value, group = group)
   ntest <- length(Tobs)
   Tsim <- replicate(nsim, Tfun(value = sample(value), group = group))
   if(ntest == 1) Tsim <- t(Tsim)
-  cbind(Tobs = Tobs, pval = rowMeans(Tsim > Tobs))
+  if(strict) {
+    out <- cbind(Tobs = Tobs, pval = rowMeans(Tsim > Tobs))
+  } else {
+    out <- cbind(Tobs = Tobs, pval = rowMeans(Tsim >= Tobs))
+  }
   ## setNames(c(Tobs, mean(Tsim > Tobs)),
   ##          nm = c("Tobs", "pval"))
 }
